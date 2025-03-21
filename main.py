340cdcd91cb53e28e2301ad1575311db109cc4e3
gabs <gmonteeeiro@gmail.com>
2025-03-21 09:19:45 -0300

    Cria métodos para verificar se está com uma reunião ativa

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 import json
-import psutil
-import time
 from dataclasses import dataclass
+
+from pycaw.pycaw import AudioUtilities, IAudioMeterInformation, AudioSession
 
 
 @dataclass
 class TheMutenatorConfig:
     meeting_app: str
     volume_downcrase: int
-    processes: list[str]
+    exceptions: list[str]
 
 
 def read_config() -> TheMutenatorConfig:
@@ -17,30 +17,63 @@ def read_config() -> TheMutenatorConfig:
 
     config = TheMutenatorConfig(
         volume_downcrase=data.get('volume_downcrase', 20),
-        processes=data.get('processes', []),
+        exceptions=data.get('exceptions', []),
         meeting_app=data.get('meeting_app', 'teams')
     )
+
+    config.exceptions.append(config.meeting_app)
 
     return config
 
 
+def full_app_name(app: str) -> str:
+    if app.endswith('.exe'):
+        return app.lower()
+    else:
+        return f'{app.lower()}.exe'
+
+
 def meeting_in_progress(config: TheMutenatorConfig) -> bool:
-    for process in psutil.process_iter(['name']):
-        if process.info['name'] == config.meeting_app:
-            try:
-                if process.net_connections(kind='inet'):
-                    return True
-            except:
-                pass
+    sessions: list[AudioSession] = AudioUtilities.GetAllSessions()
+    for session in sessions:
+        session: AudioSession
+
+        if session.Process and session.Process.name().lower() == full_app_name(config.meeting_app):
+            audio_meter = session._ctl.QueryInterface(IAudioMeterInformation)
+            audio_level = audio_meter.GetPeakValue()
+            if audio_level > 0:
+                return True
     return False
+
+
+def change_apps_volume(config: TheMutenatorConfig, volume_change: int) -> None:
+    exceptions: list[str] = [
+        full_app_name(app)
+        for app in config.exceptions
+    ]
+
+    sessions: list[AudioSession] = AudioUtilities.GetAllSessions()
+    for session in sessions:
+        session: AudioSession
+
+        if not session.Process or session.Process.name().lower() in exceptions:
+            continue
+
+        print(f'ajustar o volume do app {session.Process.name().lower()} em {volume_change}%')
 
 
 def mute_apps() -> None:
     config: TheMutenatorConfig = read_config()
+    active_meeting: bool = False
 
     while True:
-        print(f'Reunião em progesso: {meeting_in_progress(config)}')
-        time.sleep(3)
+        if meeting_in_progress(config):
+            if not active_meeting:
+                change_apps_volume(config, config.volume_downcrase * -1)
+                active_meeting = True
+        elif active_meeting:
+            change_apps_volume(config, config.volume_downcrase)
+            active_meeting = False
 
 
 if __name__ == '__main__':
